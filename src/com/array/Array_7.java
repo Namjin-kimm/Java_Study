e1fc5a14157c16cedfb378e6f0a040dae62575a8
Namjin-kimm <namjin-kim@daum.net>
2022-07-12 10:04:17 +0900

    aa

---
+++ src/com/array/Array_7.java
@@ -0,0 +1,17 @@
+package com.array;
+
+public class Array_7 {
+
+	public static void main(String[] args) {
+		int [] ar = {1, 7, 6, 0, 5};
+		//내림차순
+		
+		for(int i = 0; i < ar.length; i++) {
+			System.out.println(ar[i]);
+		}
+		
+		
+
+	}
+
+}
